d67cb0b86bd62d2a613f2038b2076699b6002bca
Uday Shankar <uday.adepu@wavemaker.com>
2024-10-21 10:42:52 +0530

    Upgraded connector code to support java 17 and compatible with wavemaker version 11.9.0

--- twilio-2fa-authy-connector-impl/src/main/java/com/wavemaker/connector/twilio/authy/service/TwilioService.java
+++ twilio-2fa-authy-connector-impl/src/main/java/com/wavemaker/connector/twilio/authy/service/TwilioService.java
@@ -5,7 +5,7 @@ import com.twilio.Twilio;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import javax.annotation.PostConstruct;
+import jakarta.annotation.PostConstruct;
 
 @Service
 public class TwilioService {
